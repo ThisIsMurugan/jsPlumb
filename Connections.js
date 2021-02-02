c21936d51bf62e761ff43f78a2e123716a13c03f
Murugan P <murugan.palaniswamy@yahoo.com>
2021-02-02 11:19:28 +0530

    reused the common styles across the connection

--- Connections.js
+++ Connections.js
@@ -4,9 +4,33 @@ var connectionInstance = jsPlumb.getInstance();
 connectionInstance.connect(
     {
         source: "someDiv",
+        target: "someDiv1"        
+    });
+
+    //Added anchors, endpoint with styles.    
+connectionInstance.connect(
+    {
+        source: "someDiv",
         target: "someDiv1",
         anchors:["Right","Left"],
         endpoint:"Rectangle",
         endpointStyle:{fill:"yellow"}
+
     });
 
+//Common styles used across the connection
+var commonStyle  = {
+    anchors:["Left","Right"],
+    endpoints:["Dot","Blank"]
+};
+
+connectionInstance.connect({
+    source:"div1",
+    target:"div2",    
+  detachable:false
+},commonStyle);
+
+connectionInstance.connect({
+    source:"div3",
+    target:"div4"
+},commonStyle);
