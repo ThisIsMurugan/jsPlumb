30db5baf0dba18aa40b58c08b8bf77b8740f6a2b
Murugan DiffPC <murugan.palaniswamy@yahoo.com>
2021-02-03 22:41:34 +0530

    loaded the data with constructor and load function

--- JsPlumbToolkit/instance.js
+++ JsPlumbToolkit/instance.js
@@ -1,5 +1,43 @@
+// create a new instance of the Toolkit by calling the newInstance method:
 var toolKit = jsPlumbToolkit.newInstance();
 
-jsPlumbToolkit.ready(function(){
+//ready function is invoked when the DOM is loaded successfully.
+jsPlumbToolkit.ready(function () {
     console.log("wow ready function invoked Get ready to play with jsPlumb toolkit edition!!!!!");
 });
+
+/*
+---------------------------------------
+Loading Data
+---------------------------------------
+*/
+var data = {
+    "nodes": [
+        { "id": "sourceElement", "name": "fooName" },
+        { "id": "targetElement", "name": "barName" }
+    ],
+    "edges": [
+        { "source": "sourceElement", "target": "targetElement" }
+    ]
+};
+
+//1-Load data during the constructor
+var instance = jsPlumbToolkit.newInstance({
+    data: data
+});
+
+//2-Load the data using load()
+toolKit.load({
+    data: {
+        "nodes": [
+            { "id": "sourceElement", "name": "fooName" },
+            { "id": "targetElement", "name": "barName" }
+        ],
+        "edges": [
+            {
+                "source": "sourceElement", "target": "targetElement"
+            }
+        ]
+    }
+});
+
