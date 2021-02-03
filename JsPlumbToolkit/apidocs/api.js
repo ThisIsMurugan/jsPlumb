1130d31c41d0279052184bac2f019ad1125ee541
Murugan DiffPC <murugan.palaniswamy@yahoo.com>
2021-02-03 22:16:22 +0530

    configured the jsPlumbToolkit edition with evaluation edition.

---
+++ JsPlumbToolkit/apidocs/api.js
@@ -0,0 +1,39 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Cluster",
+        "Edge",
+        "FaraheyInstance",
+        "Graph",
+        "Group",
+        "Layouts.Absolute",
+        "Layouts.AbsoluteBackedLayout",
+        "Layouts.AbstractHierarchicalLayout",
+        "Layouts.AbstractLayout",
+        "Layouts.EmptyLayout",
+        "Layouts.Hierarchical",
+        "Layouts.Mistletoe",
+        "Layouts.Spring",
+        "Mottle",
+        "Node",
+        "Path",
+        "Port",
+        "Selection",
+        "Surface",
+        "ZoomWidget",
+        "jsPlumb Palette directive",
+        "jsPlumbToolkit directive.",
+        "jsPlumbToolkit.Dialogs",
+        "jsPlumbToolkit.DrawingTools",
+        "jsPlumbToolkit.Model",
+        "jsPlumbToolkit.Renderers.AbstractRenderer",
+        "jsPlumbToolkit.Renderers.Miniview",
+        "jsPlumbToolkit.Support",
+        "jsPlumbToolkit.Widgets.Lasso",
+        "jsPlumbToolkitInstance"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
---
+++ JsPlumbToolkit/instance.js
@@ -0,0 +1,5 @@
+var toolKit = jsPlumbToolkit.newInstance();
+
+jsPlumbToolkit.ready(function(){
+    console.log("wow ready function invoked Get ready to play with jsPlumb toolkit edition!!!!!");
+});
